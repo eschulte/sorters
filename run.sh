79ed98f7d06d3cd65a83b02bad1cb3fe089c222e
Eric Schulte <schulte.eric@gmail.com>
2011-09-13 13:34:07 -0600

    a script for running

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# run one instance for each configuration file
+base=$(basename $(pwd))
+echo $base
+for config in *.conf;do
+    name=$(basename $config .conf)
+    echo $name
+    cd ../
+    mkdir $name
+    for sub in $config src test test.sh limit;do cp -R $base/$sub $name/; done
+    cd $name
+    echo -n "in $(pwd) "
+    echo "nice repair $config &"
+    cd ../$base
+done
