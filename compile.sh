07fc32cff3b6bb5963fdd429d66a5a18bb5ab620
Eric Schulte <schulte.eric@gmail.com>
2011-09-13 12:04:57 -0600

    compiles all the way through

--- compile.sh
+++ compile.sh
@@ -3,12 +3,20 @@ cd src/
 
 for i in bubble insertion merge quick; do
     gcc -o $i-c.s -S $i.c       # C
+    gcc -O2 -o $i-c $i-c.s
+
     g++ -o $i-cpp.s -S $i.cpp   # CPP
+    g++ -O2 -o $i-cpp $i-cpp.s
+
     ghc -o $i-hs.s -S $i.hs     # Haskell
-    # OCaml
-    ocamlopt -dstartup -S $i.ml
+    ghc -O2 -o $i-hs $i-hs.s
+
+    ocamlopt -dstartup -S $i.ml # OCaml
     mv $i.s $i-ml.s
     mv a.out.startup.s $i-startup.s
+    gcc -O2 -o $i-ml '-L/usr/lib/ocaml' $i-startup.s \
+        '/usr/lib/ocaml/std_exit.o' $i-ml.s \
+        '/usr/lib/ocaml/stdlib.a' '/usr/lib/ocaml/libasmrun.a' -lm  -ldl 
 done
 
 rm a.out *.cmx *.cmi *.o *.hi
