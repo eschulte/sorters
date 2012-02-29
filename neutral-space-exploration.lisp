8faff2919779588cce1747abf2434193c16be0ea
Eric Schulte <eric.schulte@gmx.com>
2012-02-29 01:19:53 -0700

    randomly walking the neutral network

--- neutral-space-exploration.lisp
+++ neutral-space-exploration.lisp
@@ -15,3 +15,25 @@
 (apply-path original :pos (samples-from-tracer-file "trace"))
 
 ;; walk away from the original program in genome space
+(defvar *walks* nil)
+
+(setf *walks* nil)
+
+(progn
+  (dotimes (n 1000)
+    (push (let ((ant (asm-from-file "insertion.s")) walk)
+            (dotimes (_ 100)
+              (let ((fitness (fitness ant)) neighbors)
+                (dotimes (_ 10)
+                  (let ((neighbor (copy ant)))
+                    (mutate neighbor)
+                    (push `((:fitness . ,(fitness neighbor))
+                            (:history . ,(history neighbor))) neighbors)))
+                (push `((:size      . ,(length (genome ant)))
+                        (:fitness   . ,fitness)
+                        (:history   . ,(history ant))
+                        (:neighbors . ,neighbors)) walk)
+                (mutate ant)))
+            walk) *walks*)
+    (store (car *walks*) (format nil "walk_~S.store" n)))
+  (store *walks* #P"walks.store"))
