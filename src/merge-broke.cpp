10f08317b34ef8cbf1e476c9df38eef5e1ff75c3
Eric Schulte <eric.schulte@gmx.com>
2012-07-09 15:52:13 -0600

    cpp now same bug as C

--- src/merge-broke.cpp
+++ src/merge-broke.cpp
@@ -1,47 +1,61 @@
 #include <iostream>
-#include <cmath>
 #include <stdlib.h>
-using namespace std;
+#include <string.h>
 
-void merge(int* input, int p, int r)
+int *locmerge(int *l, unsigned int ll,
+	      int *r, unsigned int lr)
 {
-  int mid = floor((p + r) / 2);
-  int i1 = 0;
-  int i2 = p;
-  int i3 = mid + 1;
-
-  // Temp array
-  int temp[r-p+1];
-
-  // Merge in sorted form the 2 arrays
-  while ( i2 <= mid && i3 <= r )
-    if ( input[i2] < input[i3] )
-      temp[i1++] = input[i2++];
-    else
-      temp[i1++] = input[i3++];
-
-  // Merge the remaining elements in left array
-  while ( i2 <= mid )
-    temp[i1++] = input[i2++];
-
-  // Merge the remaining elements in right array
-  while ( i3 <= r )
-    temp[i1++] = input[i3++];
-
-  // Move from temp array to master array
-  for ( int i = p; i <= r; i++ )
-    input[i] = temp[i-p];
-}
-
-void merge_sort(int* input, int p, int r)
-{
-  if ( p < r )
-    {
-      int mid = floor((p + r) / 2);
-      merge_sort(input, p, mid);
-      merge_sort(input, mid + 1, r);
-      merge(input, p, r);
+  int *result;
+  unsigned int il, ir, ires;
+ 
+  il = ir = ires = 0;
+ 
+  result = (int*)malloc(sizeof(int)*(ll+lr));
+  while ( (ll > 0) && ( lr > 0 ) ) {
+    if ( l[il] <= r[ir] ) {
+      result[ires++] = l[il++]; ll--;
+    } else {
+      result[ires++] = r[ir++]; lr--;
     }
+  }
+  if ( ll > 0 ) {
+    memcpy(&result[ires], &l[il], sizeof(int)*ll);
+  }
+  if ( lr > 0 ) {
+    memcpy(&result[ires], &r[ir], sizeof(int)*lr);
+  }
+  return result;
+}
+ 
+ 
+int *mergesort(int *list, unsigned int l)
+{
+  unsigned int middle;
+  int *tleft, *tright, *result, *left, *right;
+ 
+  if ( l < 2 ) return list;
+  middle = l / 2;
+  tleft = (int*)malloc(sizeof(int)*(l - middle));
+  tright = (int*)malloc(sizeof(int)*middle);
+  memcpy(tleft, list, sizeof(int)*(l-middle));
+  memcpy(tright, list+(l-middle), sizeof(int)*middle);
+  left = mergesort(tleft, l-middle);
+  right = mergesort(tright, middle);
+  if (left[l-middle-1] <= right[0])
+    result = list;
+  else
+    result = locmerge(left, l-middle, right, middle);
+  if ( tleft == left ) {
+    free(left);
+  } else {
+    free(left); free(tleft);
+  }
+  if ( tright == right ) {
+    free(right);
+  } else {
+    free(right); free(tright);
+  }
+  return result;
 }
 
 int main(int argc, char *argv[])
@@ -50,8 +64,8 @@ int main(int argc, char *argv[])
   int i;
   lst = (int*)malloc(sizeof(int)*argc-1);
   for(i=1;i<argc;i++) lst[i-1] = atoi(argv[i]);
-  merge_sort(lst, 0, (argc-1));
-  for(i=0;i<(argc-1);i++) cout << lst[i] << " ";
-  cout << endl;
+  lst = mergesort(lst, (argc-1));
+  for(i=0;i<(argc-1);i++) std::cout << lst[i] << " ";
+  std::cout << std::endl;
   return 0;
 }
