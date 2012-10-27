bd1a9c67149a55967ca621f4bac058b41a6069ef
Eric Schulte <eric.schulte@gmx.com>
2012-10-27 09:42:32 -0600

    fixed weird pthread arg passing

--- caps-multi.c
+++ caps-multi.c
@@ -15,7 +15,8 @@ int main(int argc, char *argv[]){
   struct stat sb;
   pthread_t t0,t1,t2,t3;
   int fd;
-  uint64_t size,chunk,args[2];
+  uint64_t size,chunk;
+  uint64_t a0[2],a1[2],a2[2],a3[2];
 
   fd  = open(argv[1], O_RDONLY);
   fstat(fd, &sb);
@@ -28,17 +29,18 @@ int main(int argc, char *argv[]){
 
   size  = (uint64_t)sb.st_size;
   chunk = (size / 4);
-  fprintf(stderr, "size:%lu chunk:%lu\n", size, chunk);
+  /* fprintf(stderr, "size:%lu chunk:%lu\n", size, chunk); */
 
   /* Launch Threads */
-  args[0]=0; args[1]=(1*chunk);
-  pthread_create(&t0, NULL, do_cap, (void*)args);
-  args[0]=(1*chunk)+1; args[1]=(2*chunk);
-  pthread_create(&t1, NULL, do_cap, (void*)args);
-  args[0]=(2*chunk)+1; args[1]=(3*chunk);
-  pthread_create(&t2, NULL, do_cap, (void*)args);
-  args[0]=(3*chunk); args[1]=sb.st_size;
-  pthread_create(&t3, NULL, do_cap, (void*)args);
+  a0[0]=(0*chunk); a0[1]=(1*chunk)-1;
+  a1[0]=(1*chunk); a1[1]=(2*chunk)-1;
+  a2[0]=(2*chunk); a2[1]=(3*chunk)-1;
+  a3[0]=(3*chunk); a3[1]=sb.st_size;
+
+  pthread_create(&t0, NULL, do_cap, (void*)a0);
+  pthread_create(&t1, NULL, do_cap, (void*)a1);
+  pthread_create(&t2, NULL, do_cap, (void*)a2);
+  pthread_create(&t3, NULL, do_cap, (void*)a3);
 
   /* Join Threads */
   pthread_join(t0, NULL);
@@ -47,16 +49,18 @@ int main(int argc, char *argv[]){
   pthread_join(t3, NULL);
 
   /* Write Output */
-  write(1,mem,(uint64_t)sb.st_size);
+  write(1,mem,size);
 
   return 0;
 }
 
-void *do_cap(void *args){
+void *do_cap(void *ptr){
   bool last = true;
   uint64_t i;
-  uint64_t *my_args = (uint64_t*)args;
-  for(i=my_args[0];i<my_args[1];i++)
+  uint64_t *args;
+  args = (uint64_t *)ptr;
+  /* fprintf(stderr,"%lu -> %lu\n", args[0], args[1]); */
+  for(i=args[0];i<args[1];i++)
     if (last && mem[i] <= 122 && mem[i] >= 97)
       { mem[i]-=32; last=false; }
     else
