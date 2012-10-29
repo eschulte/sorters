4aecadd655639ca01425473bef5d4b158368ec53
George Stelle <stelleg@gmail.com>
2012-10-29 12:22:08 -0600

    Improved caps-read

--- caps/caps-read.c
+++ caps/caps-read.c
@@ -1,14 +1,11 @@
 #define bufsize 2 << 15
 int main(int argc, char *argv[]){
-  int bread, fd; char buf[bufsize]; int i; char cur, prev;
+  int bread, fd; char buf[bufsize]; char *cur, *prev;
   fd = open(argv[1]);
   bread = read(fd, buf, bufsize);
   while(bread){
-    for(i=0; i<bread; i++){
-      cur = buf[i];
-      if(prev == 32 && cur >= 97 && cur <= 122) buf[i] -= 32;
-      prev = cur;
-    }
+    cur = buf+1; prev = buf;
+    while(cur < (buf + bread)){if(*prev==32 && *cur>=97 && *cur<=122) *cur -= 32; prev++; cur++;}
     write(1, buf, bread);
     bread = read(fd, buf, bufsize);
   }
