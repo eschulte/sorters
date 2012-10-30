0ec95f4d5ec3458058007ecd64ff76edfaf57494
Benjamin Edwards <bedwards@cs.unm.edu>
2012-10-30 16:57:53 -0600

    Made R read byte by byte instaed of unicode, and made it match characters exactly instead of regexp

--- caps/caps_R.R
+++ caps/caps_R.R
@@ -1,11 +1,11 @@
 #Run with Rscript caps_R.R > /dev/null
 library(multicore)
 cap_line <- function(line){
-sline<-strsplit(line," ")[[1]]
+sline<-strsplit(line," ", fixed=TRUE,useBytes=TRUE)[[1]]
 return(paste(toupper(substring(sline,1,1)),substring(sline,2),sep="",collapse=" "))}
 
 fn <- '/tmp/file';
-data<-readChar(fn,file.info(fn)$size);
-split_data <- strsplit(data,"\\n")[[1]];
+data<-readChar(fn,file.info(fn)$size,useBytes=TRUE);
+split_data <- strsplit(data,"\n",fixed=TRUE,useBytes=TRUE)[[1]];
 cs <- paste(mclapply(split_data,cap_line,mc.cores=12),sep='',collapse='\n')
 cat(cs)
