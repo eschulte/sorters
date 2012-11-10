52dcb2171da6cbddebfb152d7ac9dda88a827859
Eric Schulte <eric.schulte@gmx.com>
2012-11-10 10:15:51 -0700

    tweak

--- caps/caps.rkt
+++ caps/caps.rkt
@@ -1,15 +1,17 @@
 #lang typed/racket
 
-(: cap-forward (Char -> Char))
-(define (cap-forward prev)
-  (let: ([stdin (current-input-port)]
-         [space : Char #\Space]
-         [low-a : Char #\a])
-    (let: ([curr : (U Char EOF) (read-char stdin)])
-      (if (char? curr)
-          (begin (display (if (eq? space prev)
-                              (char-upcase curr)
-                              curr))
-                 (cap-forward curr))
-          #\newline))))
-(display (cap-forward #\Space))
+(let: ([stdin (current-input-port)]
+       [space : Char #\Space]
+       [low-a : Char #\a])
+
+  (: cap-forward (Char -> Char))
+  (define (cap-forward prev)
+  (let: ([curr : (U Char EOF) (read-char stdin)])
+    (if (char? curr)
+        (begin (display (if (eq? space prev)
+                            (char-upcase curr)
+                            curr))
+               (cap-forward curr))
+        #\newline)))
+
+  (display (cap-forward #\Space)))
