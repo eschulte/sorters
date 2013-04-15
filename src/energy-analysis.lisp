ee794e4b46c22439a58ac9da326b54f0b4e3cd6b
Eric Schulte <schulte.eric@gmail.com>
2013-04-15 17:54:39 -0600

    or... i mean this

--- src/energy-analysis.lisp
+++ src/energy-analysis.lisp
@@ -2,19 +2,9 @@
 (in-package :perf-opt)
 
 ;; robust read and process of a dirty data stream too large to fit into memory
-(defvar *ind-stats*
-  (with-open-file (in "results/energy-1/ind.stats")
-    (loop :for line = (ignore-errors (read in nil :eof)) :until (eq line :eof)
-       :when line :collect
-       (ignore-errors
-         (cons (car line)
-               (mapcar [{aget _ (cdr line)} #'car] *energy-model*))))))
-
 (with-open-file (out "/tmp/ind.data" :direction :output)
   (with-open-file (in "results/energy-1/ind.stats")
     (loop :for line = (ignore-errors (read in nil :eof)) :until (eq line :eof)
        :do (ignore-errors
-             (mapc {format out "~a ~{~a~^ ~}~%" (car line)}
-                   (remove-if-not [{> _ 2} #'length]
-                                  (mapcar [{aget _ (cdr line)} #'car]
-                                          *energy-model*)))))))
+             (format out "~a ~{~a~^ ~}~%" (car line)
+                     (mapcar [{aget _ (cdr line)} #'car] *energy-model*))))))
