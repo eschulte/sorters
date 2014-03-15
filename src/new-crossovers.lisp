5bddc358619f5b9b4206693be46ac693c6bad9d1
Eric Schulte <schulte.eric@gmail.com>
2014-03-15 15:05:38 -0600

    actually ready to run the experiment

--- src/new-crossovers.lisp
+++ src/new-crossovers.lisp
@@ -91,6 +91,9 @@
 ;; heterogeneous operations to produce functional offspring from
 ;; heterogeneous parents.
 
+(require :eager-future2)       ; Used to parallelize experimental runs
+(eager-future2:advise-thread-pool-size 24)
+
 (defun pairs (list)
   "All pairs from LIST."
   (loop :for i :below (length list) :append
@@ -104,39 +107,40 @@
             (string-downcase (symbol-name (aget :compiler first)))
             (symbol-name (aget :flag first)))))
 
+(defun runs (a b cross-op &key (number-of-runs 512))
+  "Apply CROSS-OP to A and B multiple times, returning children w/functionality."
+  (loop :for run :below number-of-runs :collect
+     (let ((mutant (funcall cross-op a b)))
+       (setf (fitness mutant) (functionalp mutant))
+       mutant)))
+
 ;; distinct pairs from mergers
 (loop :for pair :in (pairs mergers) :do
    ;; similarity metric
-   (loop :for (metric . metric-name) :in
-      (list (cons #'edit-distance :edit)
-            (cons #'component-distance :component)
-            (cons #'equality-distance :equality)) :do
+   (loop :for metric
+      :in '(edit-distance component-distance equality-distance) :do
       ;; crossover operation (with multiple synapsis context sizes)
-      (loop :for (cross-op . cross-name) :in
-         (list
-          (cons (lambda (test a b) (synapsing-crossover a b :test test :context 1))
-                :synapsing-1)
-          (cons (lambda (test a b) (synapsing-crossover a b :test test :context 2))
-                :synapsing-2)
-          (cons (lambda (test a b) (synapsing-crossover a b :test test :context 3))
-                :synapsing-3)
-          (cons (lambda (test a b) (synapsing-crossover a b :test test :context 4))
-                :synapsing-4)
-          ;; (cons (lambda (test a b) (similarity-crossover a b :test test))
-          ;;       :similarity)
-          ) :do
-         (let* ((name (format nil "~a-~a-~a-~a"
-                              (name (car pair)) (name (cdr pair))
-                              (symbol-name metric-name)
-                              (symbol-name cross-name)))
-                (path (make-pathname :name name
-                                     :type "store"
-                                     :directory '(:RELATIVE
-                                                  ".."
-                                                  "results"
-                                                  "new-crossover"))))
-           (unless (probe-file path)
-             (store (loop :for run :below 512 :collect
-                       (let ((mutant (funcall cross-op metric (car pair) (cdr pair))))
-                         (setf (fitness mutant) (functionalp mutant))
-                         mutant))))))))
+      (loop :for crossover :in '(synapsing-crossover similarity-crossover) :do
+         (loop :for context :in '(1 2 3 4) :do
+            (when (or (equal crossover 'synapsing-crossover)
+                      (equal context 1))
+              (let* ((name (format nil "~a-~a-~a-~a-~a"
+                                   (name (car pair)) (name (cdr pair))
+                                   (symbol-name metric)
+                                   (symbol-name crossover)
+                                   context))
+                     (path (make-pathname :name name
+                                          :type "store"
+                                          :directory '(:RELATIVE
+                                                       ".."
+                                                       "results"
+                                                       "new-crossover"))))
+                (eager-future2:pexec
+                  (flet ((cross (a b)
+                           (case crossover
+                             (synapsing-crossover
+                              (synapsing-crossover
+                               a b :test metric :context context))
+                             (similarity-crossover
+                              (similarity-crossover a b :test metric)))))
+                    (store (runs (car pair) (cdr pair) #'cross) path)))))))))
