df0d17ba11c89438926e2262e81dc49aff9803db
Eric Schulte <schulte.eric@gmail.com>
2014-06-17 23:09:52 -0400

    more progress on software mutational robustness

--- src/software-mutational-robustness.lisp
+++ src/software-mutational-robustness.lisp
@@ -20,19 +20,48 @@
 (defvar *llvm* (from-file (make-instance 'llvm) "sorters/merge_c.ll")
   "The original program represented using LLVM.")
 
+(defvar *asm* (from-file (make-instance 'asm) "sorters/merge_c.s")
+  "The original program represented using ASM.")
+
 #+(or )
 (defvar *work-dir* "sh-runner/work/"
   "Needed because SBCL chokes after too many shell outs.")
 
-(defun neutralp (variant)
+(defun test (variant)
   (with-temp-file (bin)
-    (ignore-errors
-      (and
-       (phenome variant :bin bin)
-       (multiple-value-bind (stdout stderr errno)
-           (shell "~a ~a" *test* bin)
-         (declare (ignorable stderr))
-         (and (zerop errno) (= (parse-integer stdout))))))))
+    (or (ignore-errors
+          (and
+           (phenome variant :bin bin)
+           (multiple-value-bind (stdout stderr errno)
+               (shell "~a ~a" *test* bin)
+             (declare (ignorable stderr))
+             (and (zerop errno) (parse-integer stdout)))))
+        -1)))
 
-(defun software-mutational-robustness (variant)
-  )
+(defun test-neutrality-and-log (output-file sorter rep)
+  "Perform a single neutrality test and log the output."
+  (with-open-file (out output-file :direction :output
+                       :if-exists :append
+                       :if-does-not-exist :create)
+    (format out "~a ~a " sorter (type-of rep))
+    (handler-case
+        (multiple-value-bind (variant edit) (mutate (copy rep))
+          (format out "~d ~a~%" (test variant) edit))
+      (error (e) (format out "~d ~a~%" -1 e)))))
+
+(defun software-mutational-robustness (output-file &key (times 1000))
+  (let* ((representations (list 'clang 'cil 'llvm 'asm))
+         (extensions (list "c" "c" "ll" "s"))
+         (sorters (list "bubble" "insertion" "merge" "quick"))
+         (all (mapcan (lambda (sorter)
+                        (mapcar (lambda (rep ext)
+                                  (cons sorter
+                                        (from-file (make-instance rep)
+                                                   (format nil "sorters/~a_c.~a"
+                                                           sorter ext))))
+                                representations extensions))
+                      sorters)))
+    (dotimes (n times)
+      (mapcar (lambda-bind ((sorter . rep))
+                (test-neutrality-and-log output-file sorter rep))
+              all))))
